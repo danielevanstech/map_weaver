722972b4656296a122e06d7ff631996a0afc40a0
Daniel Evans <daniel.evans.tech@outlook.com>
2026-09-03 10:23:11 -0700

    Initial Commit

---
+++ Map Weaver/Map_WeaverApp.swift
@@ -0,0 +1,32 @@
+//
+//  Map_WeaverApp.swift
+//  Map Weaver
+//
+//  Created by Daniel Evans on 9/3/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Map_WeaverApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Map WeaverTests/Map_WeaverTests.swift
@@ -0,0 +1,19 @@
+//
+//  Map_WeaverTests.swift
+//  Map WeaverTests
+//
+//  Created by Daniel Evans on 9/3/26.
+//
+
+import Testing
+@testable import Map_Weaver
+
+struct Map_WeaverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
